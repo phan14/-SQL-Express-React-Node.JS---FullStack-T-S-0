1d92e63c63747976b1860480f77d8af6798bc9f9
đình trần <trandangdinh@dtu.edu.vn>
2023-03-25 17:08:25 +0700

    Create Models ORM - Tạo Models/Tables Cho Dự Án

--- src/migrations/migration-project.js
+++ src/migrations/migration-project.js
@@ -1,32 +1,25 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('User', {
+    await queryInterface.createTable('Project', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+      name: {
         type: Sequelize.STRING
       },
-      password: {
+      description: {
         type: Sequelize.STRING
       },
-      username: {
+
+      startDate: {
         type: Sequelize.STRING
       },
-      address: {
-        type: Sequelize.STRING
-      },
-      sex: {
-        type: Sequelize.STRING
-      },
-      phone: {
-        type: Sequelize.STRING
-      },
-      groupId: {
+
+      customerID: {
         type: Sequelize.INTEGER
       },
 
@@ -42,6 +35,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('User');
+    await queryInterface.dropTable('Project');
   }
 };

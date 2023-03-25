7128afe795290d80f5e0c8c862733c4d85924261
đình trần <trandangdinh@dtu.edu.vn>
2023-03-25 14:47:43 +0700

    Model/Migrations/Seeder

---
+++ src/seeders/20230325074151-demo-user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Users', [
+      {
+        email: 'John Doe1',
+        password: '',
+        username: ''
+      },
+
+      {
+        email: 'John Doe 2',
+        password: '',
+        username: ''
+      },
+
+      {
+        email: 'John Doe 3',
+        password: '',
+        username: ''
+      }
+
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
-// import connection from "./config/connectDB";
+import connection from "./config/connectDb";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // test connection db
-// connection();
+connection();
 
 // init web routes
 initWebRoutes(app);
